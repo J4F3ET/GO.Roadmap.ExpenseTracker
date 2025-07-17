570e53298a77241a778a68b83380a6cab0b43061
J4F3ET <jslopezz@udistrital.edu.co>
2025-07-17 16:41:20 -0500

    feat: ✨ Agregado Prorotipo de comando help

---
+++ internal/usecase/Help.go
@@ -0,0 +1,11 @@
+package usecase
+import (
+    "fmt"
+)
+
+type HelpCommand struct{}
+
+func (c *HelpCommand) Execute(args []string) error {
+    fmt.Println("Inicializando algo con:", args)
+    return nil
+}
