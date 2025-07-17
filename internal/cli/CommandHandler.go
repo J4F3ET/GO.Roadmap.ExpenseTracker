7f6236b0e9c0dbc648d4aabcb3641ef35cf387d0
J4F3ET <jslopezz@udistrital.edu.co>
2025-07-17 16:42:02 -0500

    feat: ✨ Agregado gestor de commandos y su constructor

---
+++ internal/cli/CommandHandler.go
@@ -0,0 +1,13 @@
+package cli
+import("ExpenseTracker/internal/usecase")
+
+type CommandHandler struct {
+	commands map[string]usecase.Command
+}
+func NewCommandHandler() *CommandHandler{
+	return &CommandHandler{
+        commands: map[string]usecase.Command{
+            "help": &usecase.HelpCommand{},
+        },
+    }
+}
